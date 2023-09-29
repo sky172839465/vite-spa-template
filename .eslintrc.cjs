3b432150495724fac0e22111c833dbe0f24a3570
Yusong Hsu <sky172839465@gmail.com>
2023-09-30 00:52:23 +0800

    style: add linter and git hook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,31 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:tailwindcss/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'node_modules'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-  },
+    'no-console': 'off',
+    semi: [2, 'never'],
+    'comma-dangle': ['error', 'never'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/function-component-definition': 'off',
+    'operator-linebreak': ['error', 'after'],
+    'import/extensions': [0],
+    'import/no-extraneous-dependencies': [0]
+  }
 }
