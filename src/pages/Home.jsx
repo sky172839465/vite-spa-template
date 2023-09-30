5ab64cdeb1e69511990d15e7f2a840751307bbe6
Yusong Hsu <sky172839465@gmail.com>
2023-09-30 13:29:53 +0800

    feat: add daisyUI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom'
 const Home = () => (
   <h1 className='text-3xl font-bold underline'>
     Hello world!
+    <button
+      type='button'
+      className='inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900'
+    >
+      Button
+    </button>
     <br />
     <Link to='/about'>about</Link>
     <br />
